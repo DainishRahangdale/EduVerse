21909826f18a7a37e3688802085998d2b418f243
Dainish Rahangdale <dainish74892002@gmail.com>
2025-06-14 20:41:14 +0530

    server less added

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const Teacher = require('../routes/teacher')
-const Student = require('../routes/student');
 const cookieParser = require('cookie-parser');
+const serverless = require('serverless-http');
+
+
+const Teacher = require('../routes/teacher');
+const Student = require('../routes/student');
 const Logout = require('../routes/logout');
 const Auth = require('../routes/Auth');
 const Payment = require('../routes/payment');
@@ -11,36 +14,30 @@ const Payment = require('../routes/payment');
 const app = express();
 dotenv.config();
 
-
-app.use(cors(
-   { origin:true,
-    credentials: true,
-   }
-));
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
-
-
-app.use('/teacher',Teacher);
+app.use('/teacher', Teacher);
 app.use('/student', Student);
 app.use('/logout', Logout);
 app.use('/auth', Auth);
 app.use('/payment', Payment);
 
-
-app.get('/', (req, res)=>{
-    res.send('<p>this is server</p>')
-});
-app.post('/', (req, res)=>{
-    res.send('<p>this is server</p>')
+app.get('/', (req, res) => {
+  res.send('<p>This is server</p>');
 });
 
+app.post('/', (req, res) => {
+  res.send('<p>This is server</p>');
+});
 
 app.use((req, res) => {
-    res.status(404).send({message:'404 Not Found correct your path'});
+  res.status(404).send({ message: '404 Not Found - correct your path' });
 });
 
-app.listen(process.env.PORT,()=>{
-    console.log(`server is running on ${(process.env.PORT)}`);  
-})
+
+module.exports.handler = serverless(app);
