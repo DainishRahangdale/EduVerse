1464820f58396e23dc7eb39b25b760d87ecdbf86
Dainish Rahangdale <dainish74892002@gmail.com>
2025-06-14 20:52:04 +0530

    vercel1

--- backend/api/index.js
+++ backend/api/index.js
@@ -5,11 +5,11 @@ const cookieParser = require('cookie-parser');
 const serverless = require('serverless-http');
 
 
-const Teacher = require('../routes/teacher');
-const Student = require('../routes/student');
-const Logout = require('../routes/logout');
-const Auth = require('../routes/Auth');
-const Payment = require('../routes/payment');
+// const Teacher = require('../routes/teacher');
+// const Student = require('../routes/student');
+// const Logout = require('../routes/logout');
+// const Auth = require('../routes/Auth');
+// const Payment = require('../routes/payment');
 
 const app = express();
 dotenv.config();
@@ -21,11 +21,11 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/teacher', Teacher);
-app.use('/student', Student);
-app.use('/logout', Logout);
-app.use('/auth', Auth);
-app.use('/payment', Payment);
+// app.use('/teacher', Teacher);
+// app.use('/student', Student);
+// app.use('/logout', Logout);
+// app.use('/auth', Auth);
+// app.use('/payment', Payment);
 
 app.get('/', (req, res) => {
   res.send('<p>This is server</p>');
